5393471e163faa513d006d5e940ae11f5bf5cde0
Albert <albi@onet.eu>
2019-04-16 17:58:03 +0200

    Adds main Component of Robotic Arm

--- src/cylinderarm/CylinderArm.java
+++ src/cylinderarm/CylinderArm.java
@@ -54,7 +54,7 @@ public class CylinderArm  extends Applet implements KeyListener
     Appearance ap = new Appearance();
     Color3f TestCol = new Color3f(0.941f, 0.941f, 0.937f);
     Color3f ultramaryna = new Color3f(0.227f, 0.909f, 0.949f); //
-    //KeyPressed, keyReleased ,keyTyped metody zbierajace dane z klawiatury muszą wystąpić, skoro dodajemy KeyListener !!!
+    //KeyPressed, keyReleased ,keyTyped will allow handling inputs
     public void keyPressed(KeyEvent e) 
     {
           
@@ -75,26 +75,38 @@ public class CylinderArm  extends Applet implements KeyListener
       
     public BranchGroup createSceneGraph()
       {
-          //tworzymy glowna galaz,dodajemy prymitywy i ustawiamy ich wyglad
+          //Creating base node and adding others
           BranchGroup objRoot = new BranchGroup();
           TransformGroup transBox = new TransformGroup();
           transBox.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
-          objRoot.addChild(transBox);
-          ap.setMaterial(new Material( ultramaryna,ultramaryna,ultramaryna,ultramaryna,0.1f));
-          transBox.addChild(new Box(0.5f,0.5f,0.5f,ap));
+          TransformGroup CylinderR=new TransformGroup();
+          CylinderR.setCapability(TransformGroup.ALLOW_TRANSFORM_WRITE);
           
-          //dodajemy swiatlo
+          objRoot.addChild(transBox);
+          objRoot.addChild(CylinderR);
+          ap.setMaterial(new Material( ultramaryna,ultramaryna,ultramaryna,ultramaryna,0.1f));
+          //Adds stable, base component
+          transBox.addChild(new Box(0.5f,0.5f,0.5f,Box.GENERATE_TEXTURE_COORDS,ap));
+          //Adds Cylinder that can be rotate around own axis
+          CylinderR.addChild(new Cylinder(0.25f,5.f,Cylinder.GENERATE_TEXTURE_COORDS,ap));
+          //Adds simple texture to ap Appearance
+          TextureLoader loader = new TextureLoader("gfx/Metal_Brushed.png",null);
+          ImageComponent2D mImage = loader.getImage( );     
+          Texture2D  tx2 = new Texture2D(Texture.BASE_LEVEL, Texture.RGBA, mImage.getWidth(), mImage.getHeight());
+          tx2.setImage(0, mImage);
+          ap.setTexture(tx2);
+          //Lightning
           BoundingSphere bounds = new BoundingSphere(new Point3d(0.0,0.0,0.0), 1000.0f);
           AmbientLight alldirlight = new AmbientLight(ultramaryna);
           alldirlight.setInfluencingBounds(bounds);
-          objRoot.addChild(alldirlight); // dodamy sobie wygodne swiatlo do  ogladania obiektow z kazdej strony
+          objRoot.addChild(alldirlight); // adds light in all direction 
           Color3f lightCol = new Color3f(0.7f, 0.5f, 0.2f);
           Vector3f lightDir = new Vector3f(4.0f, -7.0f, -12.0f);
           DirectionalLight light = new DirectionalLight (lightCol,lightDir);
           light.setInfluencingBounds(bounds);
-          objRoot.addChild(light);
+          objRoot.addChild(light);// adds light in specific direction
           
-          ArmBehavior = new SimpleBehavior();
+          ArmBehavior = new SimpleBehavior(CylinderR);
           ArmBehavior.setSchedulingBounds(bounds);
           objRoot.addChild(ArmBehavior);
           
@@ -115,11 +127,11 @@ public class CylinderArm  extends Applet implements KeyListener
 
          SimpleUniverse simpleU = new SimpleUniverse(canvas3d);
         // simpleU.getViewingPlatform().setNominalViewingTransform();
-         OrbitBehavior orbit = new OrbitBehavior(canvas3d, OrbitBehavior.REVERSE_ROTATE); //tworzymy cos takiego żeby moc obracać kamere myszka       
+         OrbitBehavior orbit = new OrbitBehavior(canvas3d, OrbitBehavior.REVERSE_ROTATE); //allow rotating camera with mouse
          orbit.setSchedulingBounds(new BoundingSphere());    
          simpleU.getViewingPlatform().setViewPlatformBehavior(orbit);                        
          //simpleU.getViewingPlatform().getViewPlatformTransform();  
-         simpleU.getViewingPlatform().setNominalViewingTransform();                      //ustawiamy tak zebysmy mieli ladny widok od uruchomienia programu
+         simpleU.getViewingPlatform().setNominalViewingTransform();                      //nice view from the beggining
          simpleU.addBranchGraph(scena);
      }
 
