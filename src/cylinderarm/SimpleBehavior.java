d2771147db386c8dfe08009aa93bfdd3f453e83d
Albert <albi@onet.eu>
2019-04-17 20:38:47 +0200

    Object can now be transported by robotic arm.

--- src/cylinderarm/SimpleBehavior.java
+++ src/cylinderarm/SimpleBehavior.java
@@ -6,6 +6,7 @@
 package cylinderarm;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import static java.lang.Math.abs;
 import static java.lang.Math.cos;
 import static java.lang.Math.sin;
 import static java.lang.Math.toRadians;
@@ -35,6 +36,9 @@ public class SimpleBehavior extends Behavior
     public float Arm_Pos;
     public float Gripper_PosL=-0.4f;
     public float Gripper_PosR=0.4f;
+    public float TestObj_Angle=0.0f;
+    public float TestObj_Pos=2.0f;
+    public float TestObj_PosY=2.0f;
     //will store information about input, [0]=Left Arrow[1]=RightArrow,[2] up,[3] down[4]=<,[5]=>,[6] k,[7] l
     public boolean[] IsKeyPressed = new boolean[8];
     public float CylinderR_Angle=0.f;
@@ -67,6 +71,8 @@ public class SimpleBehavior extends Behavior
     {
         if(IsKeyPressed[0]) { CylinderR_Angle+=0.01f;}
         if(IsKeyPressed[1]) CylinderR_Angle-=0.01f;
+        if(abs(CylinderR_Angle)>6.28) CylinderR_Angle=0.0f;
+       
     }
     
     public void CheckForArm()
@@ -170,28 +176,57 @@ public class SimpleBehavior extends Behavior
         Transform3D setUp = new Transform3D();
         Transform3D Rot = new Transform3D();
         Transform3D Fin = new Transform3D();
-        setUp.setTranslation(new Vector3f(1.5f,1.f,3.f));
-        //Rot.rotY(CylinderR_Angle);
+        if(IsInRange())
+        {
+        setUp.setTranslation(new Vector3f(0.0f,handlepos,1.5f+Arm_Pos+2.0f));
+        Rot.rotY(CylinderR_Angle);
+        Fin.mul(Rot,setUp);
+        TestObj_Pos=1.5f+Arm_Pos+2.0f;
+        TestObj_Angle = CylinderR_Angle;
+        TestObj_PosY=handlepos;
+        TestObj.setTransform(Fin);
+            
+        }
+        else
+        {
+          if(abs(TestObj_Angle)>6.25f) TestObj_Angle=0.0f;
+        setUp.setTranslation(new Vector3f(0.f,TestObj_PosY,TestObj_Pos));
+        Rot.rotY(TestObj_Angle);
         Fin.mul(Rot,setUp);
         
-        TestObj.setTransform(setUp);
+        TestObj.setTransform(Fin);
+        }
+       
         
+    }
+    public boolean IsInRange()
+    {
+       
+       if (CylinderR_Angle>=0.0f&&CylinderR_Angle >= TestObj_Angle-0.07f && CylinderR_Angle <= TestObj_Angle+0.07f &&abs(Gripper_PosR)<0.4f&&handlepos>=TestObj_PosY-0.15f&&handlepos<=TestObj_PosY+0.15f)
+               return true; 
+       else if(CylinderR_Angle<= TestObj_Angle+0.07f && CylinderR_Angle>= TestObj_Angle-0.07f&&abs(Gripper_PosR)<0.4f&&handlepos>=TestObj_PosY-0.25f&&handlepos<=TestObj_PosY+0.25f)
+                {
+                   return true;          
+                }    
+       return false;
     }
      private void update()
          {
+             //checking input data
              CheckForRotation();
              CheckForHandleMovement();
              CheckForArm();
              CheckForGripperMove();
+             //setting posoition of robotic arm components
              SetCylinder();
              SetHandle();
              SetArm();
              SetGripperBase();
              SetGripperLeft();
              SetGripperRight();
+             //setting position of test objects
              SetTestObj();
-          
-            
              
+            
          }
 }
