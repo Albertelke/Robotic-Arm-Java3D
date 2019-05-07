4bf0e87c29f972415fc9a9f1634a7e46c9601915
Albert <albi@onet.eu>
2019-05-08 01:44:09 +0200

    Add class that store movement information

---
+++ src/cylinderarm/MovementContainer.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cylinderarm;
+
+/**
+ *
+ * @author Albert
+ */
+public class MovementContainer 
+{
+    public float handlepos;
+    public float Arm_Pos;
+    public float Gripper_PosL;
+    public float Gripper_PosR;
+
+    MovementContainer(float handlepos,float Arm_Pos,float Gripper_PosL,float Gripper_PosR)
+    {
+    this.handlepos=handlepos;
+    this.Arm_Pos=Arm_Pos;
+    this.Gripper_PosL=Gripper_PosL;
+    this.Gripper_PosR=Gripper_PosR;
+   
+    }
+}
