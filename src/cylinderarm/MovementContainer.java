31c01944d838df2b0b49a8ddf527f1ab5815e4e6
Albert <albi@onet.eu>
2019-05-13 18:38:54 +0200

    Bug fixed

--- src/cylinderarm/MovementContainer.java
+++ src/cylinderarm/MovementContainer.java
@@ -15,13 +15,15 @@ public class MovementContainer
     public float Arm_Pos;
     public float Gripper_PosL;
     public float Gripper_PosR;
-
-    MovementContainer(float handlepos,float Arm_Pos,float Gripper_PosL,float Gripper_PosR)
+    float CylinderR_Angle;
+    
+    MovementContainer(float handlepos,float Arm_Pos,float Gripper_PosL,float Gripper_PosR,float CylinderR_Angle)
     {
     this.handlepos=handlepos;
     this.Arm_Pos=Arm_Pos;
     this.Gripper_PosL=Gripper_PosL;
     this.Gripper_PosR=Gripper_PosR;
+    this.CylinderR_Angle=CylinderR_Angle;
    
     }
 }
